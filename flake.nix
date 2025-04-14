ecbf8c967e68b4ef2c57055c704389609dddcc66
Ayase <166034664+AyaseFile@users.noreply.github.com>
2025-04-14 21:55:51 +0800

    refactor: rm pkgs, services

--- flake.nix
+++ flake.nix
@@ -21,21 +21,12 @@
     let
       inherit (builtins) elem filter;
       inherit (nixpkgs.lib) genAttrs replaceStrings;
-      inherit (nixpkgs.lib.filesystem) listFilesRecursive packagesFromDirectoryRecursive;
-
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-darwin"
-      ];
-      forAllSystems = f: genAttrs supportedSystems (system: f (import nixpkgs { inherit system; }));
+      inherit (nixpkgs.lib.filesystem) listFilesRecursive;
 
       nameOf = path: replaceStrings [ ".nix" ] [ "" ] (baseNameOf (toString path));
 
       moduleFiles = listFilesRecursive ./modules;
       modules = map nameOf moduleFiles;
-
-      serviceFiles = listFilesRecursive ./services;
-      services = map nameOf serviceFiles;
 
       specialModules = [
         "secureboot"
@@ -45,17 +36,8 @@
       ];
     in
     {
-      packages = forAllSystems (
-        pkgs:
-        packagesFromDirectoryRecursive {
-          inherit (pkgs) callPackage;
-          directory = ./packages;
-        }
-      );
-
-      nixosModules =
+      modules =
         (genAttrs (filter (name: !(elem name specialModules)) modules) (name: import ./modules/${name}.nix))
-        // (genAttrs services (name: import ./services/${name}.nix))
         // {
           secureboot = import ./modules/secureboot.nix { inherit lanzaboote; };
           vscode-server = import ./modules/vscode-server.nix { inherit vscode-server; };
